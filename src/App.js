4113cf86d516418687e05c018945fe832b5173bf
Evan <ewlusky@gmail.com>
2018-07-11 10:47:52 -0500

    a commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import Kennel from './Kennel.js';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+      <Kennel />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/LocationList.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+
+export default class LocationList extends Component {
+    render() {
+        return (
+            <div>
+                <h4>Nashville North Location</h4>
+                <h5>500 Puppy Way</h5>
+                <h4>Nashville South Location</h4>
+                <h5>401 Kitty Blvd.</h5>
+            </div>
+        );
+    }
+}
