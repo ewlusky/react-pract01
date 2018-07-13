d388f39ac20d3f419748267e29b5556e4f82ae40
Evan <ewlusky@gmail.com>
2018-07-13 10:42:02 -0500

    remember me

--- src/ApplicationViews.js
+++ src/ApplicationViews.js
@@ -4,18 +4,37 @@ import AnimalList from './AnimalList'
 import Animal from './Animal'
 import EmployeeList from './EmployeeList'
 import LocationList from './LocationList'
+import Login from './Login'
+
 export default class ApplicationViews extends Component {
+
+    isAuthenticated = () => {
+        return (localStorage.getItem("credentials") !== null || sessionStorage.getItem("credentials") !== null)
+    }
+
     render() {
         return (
+
             <React.Fragment>
-                <Route exact path="/" component={LocationList} />
-                <Route exact path="/animals" component={AnimalList} />
-                <Route path="/animals/:animalId" render={(props) => {
-                    console.log(props)
-                    return <Animal animal={props.location.state.animal} />
+                <Route path="/" render={props => {
+                    if (!(this.isAuthenticated())) {
+                        return <Login />
+                    } else {
+                        return (
+                            <React.Fragment>
+
+                                <Route exact path="/" component={LocationList} />
+                                <Route exact path="/animals" component={AnimalList} />
+                                <Route path="/animals/:animalId" render={(props) => {
+                                    return <Animal animal={props.location.state.animal} />
+                                }} />
+                                <Route path="/employees" component={EmployeeList} />
+                                <Route path="/logi" component={Login} />
+                            </React.Fragment>
+                        )
+                    }
                 }} />
-                {/* <Route path="/animals/:animalId" component={Animal} /> */}
-                <Route path="/employees" component={EmployeeList} />
+
             </React.Fragment>
         )
     }
